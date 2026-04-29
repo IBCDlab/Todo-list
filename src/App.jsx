69f18bf193aa7e603afcd88ae80cc266916fcb5b
Ina Bryzheuskaya <inabri783@gmail.com>
2026-04-28 21:34:59 -0500

    Add TodoForm submit handler

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,18 @@ import TodoForm from "./TodoForm.jsx";
 import TodoList from "./TodoList.jsx";
 import "./App.css";
 
-const todos = [
-  { id: 1, title: "review resources" },
-  { id: 2, title: "take notes" },
-  { id: 3, title: "code out app" },
-];
-
 function App() {
-  const [todoList, setTodoList] = useState(todos);
+  const [todoList, setTodoList] = useState([]);
+  function addTodo(todoTitle) {
+    const newTodo = { id: Date.now(), title: todoTitle };
+
+    setTodoList((previos) => [newTodo, ...previos]);
+  }
   return (
     <div>
       <h1>Todo List</h1>
-      <TodoForm />
-      <TodoList todoList={todoList} />{" "}
+      <TodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
