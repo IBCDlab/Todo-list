3e9be44c75e81f115a416949af62aab40bf52986
Ina Bryzheuskaya <inabri783@gmail.com>
2026-05-08 18:58:45 -0500

    Use ternary operator in TodoList

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -2,19 +2,23 @@ import TodoListItem from "./TodoListItem";
 
 function TodoList({ todoList, onCompleteTodo }) {
   const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
-  if (filteredTodoList.length === 0) {
-    return <p>Add todo above to get started</p>;
-  }
+
   return (
-    <ul>
-      {filteredTodoList.map((todo) => (
-        <TodoListItem
-          key={todo.id}
-          todo={todo}
-          onCompleteTodo={onCompleteTodo}
-        />
-      ))}
-    </ul>
+    <>
+      {filteredTodoList.length === 0 ? (
+        <p>Add todo above to get started</p>
+      ) : (
+        <ul>
+          {filteredTodoList.map((todo) => (
+            <TodoListItem
+              key={todo.id}
+              todo={todo}
+              onCompleteTodo={onCompleteTodo}
+            />
+          ))}
+        </ul>
+      )}
+    </>
   );
 }
 
