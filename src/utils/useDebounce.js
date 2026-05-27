59e30ce9ae4709a9626cb4cd0f0455ffd5dad275
Ina Bryzheuskaya <inabri783@gmail.com>
2026-05-27 16:12:29 -0500

    Add todo search and error handling

---
+++ src/utils/useDebounce.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+export default function useDebounce(value, delay) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const timeoutId = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
